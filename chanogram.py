8b774e10f7e3fffab03fbc64510a8ca6efd4b041
Abdul Magnun <abdulmagnun@gmail.com>
2016-07-04 08:31:20 +0200

    Min replies 100 + add this info to sub msg

--- chanogram.py
+++ chanogram.py
@@ -25,8 +25,8 @@ class Chanogram:
                  db_file = 'chanogram.db',
                  settings = {'board': 'pol',
                              'filter_list': ['edition',],
-                             'min_replies': 50,
-                             'min_rpm': 3.9}):
+                             'min_replies': 100,
+                             'min_rpm': 4.4}):
 
         logging.debug('Attempting to init Chanogram instance...')
         with open(api_token_file, 'r') as f:
@@ -160,12 +160,13 @@ _Use_ /stop _if you want to unsubscribe._'''
 
 You will receive a *notification* if a *thread* on 4chan's /{0}/ board is attracting *lots of responses in a short time*.
 
-Currently, a notification is triggered when a thread attracts *more than {1} replies per minute* and does not have the following keyword{2}: "{3}".
+Currently, a notification is triggered when a thread attracts *more than {1} replies per minute*, has at least {2} replies and does not have the following keyword{3}: "{4}".
 _(since those threads usually aren't related to breaking news but attract lots of replies per minute because of regular posters)_
 
 _Use_ /stop _to unsubscribe._'''\
                 .format(self.settings['board'],
                         str(self.settings['min_rpm']),
+                        str(self.settings['min_replies']),
                         plural_handler,
                         ', '.join(self.settings['filter_list']))
             self.bot.sendMessage(from_id, reply, parse_mode='Markdown')
