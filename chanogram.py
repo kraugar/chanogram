307dc44192000e9cd094ae7071ca9c58f93181a5
Abdul Magnun <abdulmagnun@gmail.com>
2016-07-04 08:35:55 +0200

    Sub msg formatting

--- chanogram.py
+++ chanogram.py
@@ -160,8 +160,7 @@ _Use_ /stop _if you want to unsubscribe._'''
 
 You will receive a *notification* if a *thread* on 4chan's /{0}/ board is attracting *lots of responses in a short time*.
 
-Currently, a notification is triggered when a thread attracts *more than {1} replies per minute*, has *at least {2} replies* and does not have the following keyword{3}: {4}.
-_(since those threads usually aren't related to breaking news but attract lots of replies per minute because of regular posters)_
+Currently, a notification is triggered when a thread attracts *more than {1} replies per minute*, has *at least {2} replies* and does not have the following keyword{3}: *{4}* _(since those threads usually aren't related to breaking news but attract lots of replies per minute because of regular posters)_
 
 _Use_ /stop _to unsubscribe._'''\
                 .format(self.settings['board'],
