a264bcf1588a13e95b9197fb8d5db762cc9eeb70
Abdul Magnun <abdulmagnun@gmail.com>
2016-07-08 11:15:18 +0200

    Fixed critical bug related to checking read msgs

--- chanogram.py
+++ chanogram.py
@@ -3,7 +3,7 @@ import sqlite3
 import telepot
 import time
 import subprocess
-import logging
+import logging, logging.handlers
 import exceptions
 from dateutil.relativedelta import relativedelta
 from BeautifulSoup import BeautifulSoup
@@ -14,10 +14,25 @@ from urllib2 import urlopen
 with open('admin_id') as f:
     admin_id = f.read()
 
-logging.basicConfig(filename='chanogram.log',
-                    level=logging.DEBUG,
-                    filemode='w',
-                    format='%(asctime)s %(levelname)s %(message)s')
+logger = logging.getLogger('chanogram')
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter(\
+            '%(asctime)s - %(levelname)s - %(message)s')
+fhd = logging.handlers.TimedRotatingFileHandler(\
+     'logs/chanogram-debug.log', when='H',interval=24,backupCount=7)
+fhi = logging.handlers.TimedRotatingFileHandler(\
+     'logs/chanogram-info.log', when='H',interval=24,backupCount=7)
+ch = logging.StreamHandler()
+fhd.setLevel(logging.DEBUG)
+fhd.setFormatter(formatter)
+fhi.setLevel(logging.DEBUG)
+fhi.setFormatter(formatter)
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+logger.addHandler(fhd)
+logger.addHandler(fhi)
+
 
 class Chanogram:
     def __init__(self,
@@ -28,13 +43,13 @@ class Chanogram:
                              'min_replies': 100,
                              'min_rpm': 4.4}):
 
-        logging.debug('Attempting to init Chanogram instance...')
+        logger.debug('Attempting to init Chanogram instance...')
         with open(api_token_file, 'r') as f:
             self.api_token = f.read()
         self.bot = telepot.Bot(self.api_token)
         self.bot.message_loop(self.handle_input)
 
-        logging.debug('Attempting to init Chanogram database...')
+        logger.debug('Attempting to init Chanogram database...')
         self.db_file = db_file
         self.conn = sqlite3.connect(self.db_file)
         self.c = self.conn.cursor()
@@ -52,11 +67,11 @@ class Chanogram:
         self.bot.sendMessage(admin_id,
                              'Deployed with last update:\n{0}'\
                              .format(last_commit))
-        logging.info('Chanogram instance init complete.')
+        logger.debug('Chanogram instance init complete.')
 
 
     def list_add(self, list_name, entry):
-        logging.debug('Attempting to add {0} to list {1}...'\
+        logger.debug('Attempting to add {0} to list {1}...'\
                       .format(entry, list_name))
         self.conn = sqlite3.connect(self.db_file)
         self.c = self.conn.cursor()
@@ -64,11 +79,11 @@ class Chanogram:
         "INSERT OR IGNORE INTO {0} VALUES ('{1}')".format(list_name, entry))
         self.conn.commit()
         self.conn.close()
-        logging.debug('Added {0} to list {1}.'.format(entry, list_name))
+        logger.debug('Added {0} to list {1}.'.format(entry, list_name))
 
 
     def list_del(self, list_name, entry):
-        logging.debug('Attempting to delete {0} from list {1}...'\
+        logger.debug('Attempting to delete {0} from list {1}...'\
                       .format(entry, list_name))
         self.conn = sqlite3.connect(self.db_file)
         self.c = self.conn.cursor()
@@ -76,18 +91,21 @@ class Chanogram:
         "DELETE FROM {0} WHERE entry='{1}'".format(list_name, entry))
         self.conn.commit()
         self.conn.close()
-        logging.debug('Deleted {0} from list {1}.'.format(entry, list_name))
+        logger.debug('Deleted {0} from list {1}.'.format(entry, list_name))
 
 
     def list_get(self, list_name):
-        logging.debug('Attempting to get list {0}...'.format(list_name))
+        logger.debug('Attempting to get list {0}...'.format(list_name))
         self.conn = sqlite3.connect(self.db_file)
         self.c = self.conn.cursor()
         entries = []
         for e in self.c.execute("SELECT * FROM {0}".format(list_name)):
-            entries.append(e[0])
+            entries.append(str(e[0]))
         self.conn.close()
-        logging.debug('Got list {0}.'.format(list_name))
+        logger.debug('Got list {0} with {1} entries, last being: {2}.'
+                     .format(list_name,
+                             len(entries),
+                             ', '.join(entries[-5:])))
         return entries
 
 
@@ -95,7 +113,7 @@ class Chanogram:
         global admin_id
         from_id = str(msg['from']['id'])
         text = msg['text']
-        logging.debug('Attempting to handle message from {0}: "{1}"...'\
+        logger.debug('Attempting to handle message from {0}: "{1}"...'\
                       .format(from_id, text[:20]))
 
 
@@ -119,6 +137,10 @@ class Chanogram:
         elif text == '/subs' and from_id == admin_id:
             self._subs(admin_id)
 
+        elif text.startswith('/broadcast_manually ') and from_id == admin_id:
+            text = text[len('/broadcast_manually '):]
+            self._broadcast_manually(text)
+
 
         else:
             if from_id == admin_id:
@@ -138,7 +160,7 @@ class Chanogram:
 /ping to _receive a pong_.'''
                 self.bot.sendMessage(from_id, reply, parse_mode='Markdown')
 
-        logging.debug('Message handled from {0}: {1}'\
+        logger.debug('Message handled from {0}: {1}'\
                       .format(from_id, text[:40]))
 
 
@@ -172,7 +194,7 @@ _Use_ /stop _to unsubscribe._'''\
                         plural_handler,
                         '; '.join(self.settings['filter_list']))
             self.bot.sendMessage(from_id, reply, parse_mode='Markdown')
-            logging.info('User with ID {0} subscribed.'.format(from_id))
+            logger.info('User with ID {0} subscribed.'.format(from_id))
 
 
     def _stop(self, from_id):
@@ -182,7 +204,7 @@ _Use_ /stop _to unsubscribe._'''\
 '''You have *unsubscribed*.
 _Use_ /start _to subscribe again._''',
                                  parse_mode='Markdown')
-            logging.info('User with ID {0} unsubscribed.'.format(from_id))
+            logger.info('User with ID {0} unsubscribed.'.format(from_id))
 
         else:
             self.bot.sendMessage(from_id,
@@ -192,67 +214,54 @@ _Use_ /start _to subscribe again._''',
 
 
     def _log(self, admin_id):
-        loglevel = logging.getLogger().getEffectiveLevel()
-        if loglevel == 10:
-            lines = 100
-        else:
-            lines = 20
-        logtail = subprocess.check_output('tail -n {0} chanogram.log'\
-                                          .format(lines),
+        logtail = subprocess.check_output('tail -n 50 logs/chanogram-debug.log',
                                           shell=True)
         self.bot.sendMessage(admin_id, logtail[-4000:].replace('\n','\n\n'))
-        logging.debug(\
-                      'Sent logtail to admin with ID {0}.'\
-                      .format(admin_id))
-
-
-    def _debug(self, admin_id):
-        logging.getLogger().setLevel(logging.DEBUG)
-        reply =\
-'DEBUG MODE ENABLED for up to 30 seconds. Use /log to check the output.'
-        self.bot.sendMessage(admin_id, reply)
-
+        time.sleep(0.5)
+        logtail = subprocess.check_output('tail -n 50 logs/chanogram-info.log',
+                                          shell=True)
+        self.bot.sendMessage(admin_id, logtail[-4000:].replace('\n','\n\n'))
 
     def _subs(self, admin_id):
         subs = self.list_get('subscribers')
         if len(subs) > 1:
             plural_handler = '''Currently there are {0} subscribers:\n{1}'''
-        else:
+        elif len(subs) == 1:
             plural_handler = '''Currently there is one subscriber: {1}'''
+        else:
+            plural_handler = '''Currently there are no subscribers.'''
         reply = plural_handler.format(len(subs), '\n'.join(subs))
         self.bot.sendMessage(admin_id, reply, parse_mode='Markdown')
-        logging.debug(\
-            'Sent subscribers list with {0} entries to admin with ID {1}.'\
-            .format(len(subs), admin_id))
+
+    def _broadcast_manually(self, text):
+        subs = self.list_get('subscribers')
+        for sub in subs:
+            self.bot.sendMessage(sub, text)
 
 
     def broadcast(self, msg):
-        logging.debug('Attempting to broadcast message: "{0}"...'\
-                      .format(msg[:40]))
         subs = self.list_get('subscribers')
         for sub in subs:
             self.bot.sendMessage(sub, msg, parse_mode='Markdown')
-        logging.info('Broadcasted message to {0} subscribers: "{1}".'\
+        logger.info('BROADCASTED message to {0} subscribers: "{1}".'\
                      .format(len(subs), msg[:40]))
 
 
     def get_threads(self):
-        logging.debug('Attempting to get API file for /{0}/ board...'
-                      .format(self.settings['board']))
         api_file = urlopen('https://a.4cdn.org/{0}/catalog.json'\
                            .format(self.settings['board'])).read()
       #  with open('catalog.json', 'r') as f:
      #       api_file = f.read()
         api_json = json.loads(api_file)
-        logging.debug('Got API file.')
+        logger.debug('Got API file for /{0}/.'.format(self.settings['board']))
 
-        logging.debug('Attempting to process threads...')
         threads = []
         for page in api_json:
             for thread in page['threads']:
                 thread['board'] = self.settings['board']
                 thread['url'] = 'http://boards.4chan.org/{0}/thread/{1}'\
                                 .format(self.settings['board'], thread['no'])
+                thread['no'] = str(thread['no'])
 
                 thread['text'] = 'No text available'
                 if 'name' in thread:
@@ -286,17 +295,15 @@ _Use_ /start _to subscribe again._''',
 
                 threads.append(thread)
 
-        logging.debug('Attempting to remove read threads...')
         unread = []
         read = self.list_get('broadcast_history')
         for thread in threads:
-            if thread['id'] not in read:
+            if thread['no'] not in read:
                 unread.append(thread)
-        logging.debug('Removed {0} read threads.'\
+        logger.debug('Removed {0} threads already broadcasted.'\
                       .format(len(threads) - len(unread)))
         threads = unread
 
-        logging.debug('Attempting to filter threads...')
         i = 0
         r = 0
         words = self.settings['filter_list']
@@ -307,17 +314,16 @@ _Use_ /start _to subscribe again._''',
                 r = r + 1
             else:
                 i = i + 1
-        logging.debug('Filtered out {0} threads.'.format(r))
+        logger.debug('Removed {0} threads matching filter list.'.format(r))
 
         threads.sort(key=lambda t: t['rpm'],reverse = True)
 
-        logging.debug('Finally arrived at {0} threads from board /{1}/ .'\
+        logger.debug('Finally arrived at {0} threads from board /{1}/ .'\
                       .format(len(threads), self.settings['board']))
         return threads
 
 
     def format_thread(self, thread):
-        logging.debug('Formatting thread...')
         thread = '*{0}/min ({1}r in {2})*\n{3}\n\n(from {4})\n{5}'\
                  .format(thread['rpm'],
                          thread['replies'],
@@ -325,37 +331,38 @@ _Use_ /start _to subscribe again._''',
                          thread['text'],
                          thread['country_name'],
                          thread['url'])
-        logging.debug('Thread formatted.')
+        logger.debug('Thread formatted.')
         return thread
 
 
     def run(self):
         try:
-            logging.debug('Attempting a check operation...')
+            logger.debug('-> Attempting a check operation...')
             threads = self.get_threads()
             t = threads[0]
 
             if float(t['replies']) > self.settings['min_replies'] and\
                t['rpm'] > self.settings['min_rpm']:
-                if t['id'] not in self.list_get('broadcast_history'):
-                    formatted_thread = self.format_thread(t)
-                    self.broadcast(formatted_thread)
-                    self.list_add('broadcast_history', str(t['id']))
-                else:
-                    logging.debug('Already broadcasted message: {0}'\
-                                  .format(t['id']))
+                formatted_thread = self.format_thread(t)
+                self.broadcast(formatted_thread)
+                self.list_add('broadcast_history', t['no'])
             else:
-                percentage = "%.1f" % (t['rpm'] * 100 /
-                                       self.settings['min_rpm'])
-                logging.debug('No threads matching requirements, closest at {0}/min ({1}%): "{2}"'.format(t['rpm'], percentage, t['text'][:30]))
-            logging.debug('Check operation complete.')
+                perc_rpm = "%.1f" % (t['rpm'] * 100 / self.settings['min_rpm'])
+                perc_replies = (int(t['replies']) * 100 /
+                                int(self.settings['min_replies']))
+                logger.debug('No threads matching requirements, '
+                             'closest at {0}/min ({1}%) with {2} posts ({3}%)'
+                             ': "{4}"'\
+                             .format(t['rpm'], perc_rpm,
+                                     t['replies'], perc_replies,
+                                     t['text'][:30]))
+            logger.debug('-> Check operation complete.')
         except exceptions.Exception as e:
             print e
-            logging.error('Check operation failed with error: ', e)
+            logger.error('!!! Check operation failed with error: ', e)
 
 
 c = Chanogram()
 while True:
     c.run()
     time.sleep(30)
-    logging.getLogger().setLevel(logging.INFO)
